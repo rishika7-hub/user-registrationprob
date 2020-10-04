2cd75641f80ac6dfe197f082d6934e395b95e754
rishika7-hub <srishika970@gmail.com>
2020-10-04 23:13:39 +0530

    First name

--- userRegistration.sh
+++ userRegistration.sh
@@ -1,3 +1,12 @@
 #!/bin/bash 
 echo "Welcome to User Registration Problem"
 
+read -p "Enter the first name:" fname
+pat="([A-Z][a-z]*)*"
+
+if [[ $fname =~ $pat ]]
+then
+echo "Valid first name"
+else
+echo "Invalid first name"
+fi
