ae22bb3974edb267e1db03d2029a5ec4087db615
rishika7-hub <srishika970@gmail.com>
2020-10-04 23:44:31 +0530

    Valid email id

--- userRegistration.sh
+++ userRegistration.sh
@@ -19,3 +19,11 @@ echo "Valid last name"
 else
 echo "Invalid last name"
 fi
+
+read -p "Enter Email ID: " email
+if [[ "$email" =~ [a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z] ]]
+then
+        echo "Email address is valid"
+else
+        echo "Please enter a valid email address"
+fi
